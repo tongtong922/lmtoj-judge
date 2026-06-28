d92fb8209631f42c0ab503965bbc25894f75b965
limaotong <82311072@qq.com>
2026-06-28 19:24:08 +0800

    提交评测 #7 (题目 1001)

---
+++ submit/7.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define fi first
+#define se second
+#define pii pair<int,int>
+#define lowbit(x) ((x)&(-(x)))
+#define popc(x) __builtin_popcountll(x)
+#define ctz(x) __builtin_ctzll(x)
+#define clz(x) __builtin_clzll(x)
+#define double long double
+#define sqrt(x) sqrtl(x)
+#define cbrt(x) cbrtl(x)
+#define pow(x,y) powl(x,y)
+#define sin(x) sinl(x)
+#define cos(x) cosl(x)
+#define tan(x) tanl(x)
+#define vct basic_string
+const int N=5e5+10,mod=998244353;
+signed main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0),cout.tie(0);
+	int n=1145;
+	while(n--) cout<<"CCFILOVEYOU";
+	return 0;
+}
