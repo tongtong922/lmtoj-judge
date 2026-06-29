da97ee865cb77ec42f6d33277eb21060f5ca8bf5
limaotong <82311072@qq.com>
2026-06-29 18:54:31 +0800

    提交评测 #11 (题目 1001)

---
+++ submit/11.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n;
+
+signed main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n;
+    for (int i = 1;i <= n;i++){
+        cout << "CCFILOVEYOU";
+    }
+    return 0;
+}
