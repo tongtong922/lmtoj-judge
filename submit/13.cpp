3a5833e1ece049f44238b9c02159538d9ac7ed9e
limaotong <82311072@qq.com>
2026-06-30 22:31:57 +0800

    提交评测 #13 (题目 1001)

---
+++ submit/13.cpp
@@ -0,0 +1,26 @@
+// Copyright 2026 (C) Gary0
+// All rights reserved
+#define debug
+#include <bits/stdc++.h>
+#define inf 0x3f3f3f3f
+typedef long long LL;
+using namespace std;
+
+int t;
+
+int n;
+bool a[1003][1003];
+
+int main()
+{
+	ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+	cin >> t;
+	while (t--)
+	{
+		cin >> n;
+		for (int i = 1; i <= n; i++)
+			for (int j = 1; j <= n; j++) cin >> a[i][j];
+		
+	}
+	return 0;
+}
